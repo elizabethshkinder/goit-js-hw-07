8c0015c78b596c741d6a44c12954f9944d5eec76
Elizabeth Shkinder <elizabethshkinder@mac.home>
2025-09-28 14:47:42 +0100

    add solution for task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const {email, password} = event.currentTarget.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (!emailValue || !passwordValue){
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const data = {
+        [email.name]: emailValue,
+        [password.name]: passwordValue,
+    }
+
+    console.log (data);
+
+    event.currentTarget.reset();
+
+});
+
+
+
+
+
 
