8d6bb42420245904fc00d9c52991a0033de1634a
Elizabeth Shkinder <elizabethshkinder@mac.home>
2025-09-28 11:29:49 +0100

    add solution for task-1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector("#categories");
+const items = categories.querySelectorAll(".item");
+
+console.log(`Number of categorie: ${items.length}`);
+
+items.forEach((item) => {
+    const title = item.querySelector("h2").textContent;
+    const elemCount = item.querySelectorAll("ul li").length;
+
+    console.log(`Categories: ${title}`);
+    console.log(`Elements: ${elemCount}`);
+});
+
+
